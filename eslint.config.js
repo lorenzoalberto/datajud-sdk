2e65354afe23bbd97394211fc354c2d5c234e05f
Lorenzo Alberto <lorenzoalberto02@gmail.com>
2026-07-29 09:17:20 -0300

    chore(config): configura ambiente de desenvolvimento TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/dist/**",
+      "coverage",
+      "eslint.config.js",
+      "frontend/server.mjs",
+      "**/*.config.js",
+      "**/*.config.d.ts",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  { languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } } },
+  eslintConfigPrettier,
+);
